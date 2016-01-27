fe297d70f84c9d90617c73a99b5822161c4ebaf5
ivanmauricio <ivangh@hotmail.co.uk>
2016-01-27 17:47:06 +0000

    added stop button

--- stopwatch.js
+++ stopwatch.js
@@ -1,25 +1,26 @@
 var displayTime = 0;
 var offset;
+var intervalFunction;
 var timeStopped;
 var timeElapsed;
 
 function startTimer() {
-  offset = Date.now();
-  setInterval(display, 10);
+  offset = timeStopped || Date.now();
+  intervalFunction = setInterval(display, 1000);
 }
 
 function stopTimer() {
-
+  clearInterval(intervalFunction);
 }
 
 function timeDifference() {
   var now = Date.now();
   var timeDiff = now - offset;
+  offset = now;
   return timeDiff;
 }
 
 function display() {
-  var timeDiff = timeDifference();
-  displayTime =+ timeDiff;
+  displayTime += timeDifference();
   console.log(displayTime);
 }
